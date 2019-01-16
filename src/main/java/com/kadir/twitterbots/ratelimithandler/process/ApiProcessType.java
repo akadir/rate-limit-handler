ddae83c1f5f3585a9588b20bb3032fc3c05c63d5
kadir <akadirkkoc@gmail.com>
2019-01-16 23:51:30 +0300

    Add delayInSecond parameter in ApiProcessType Enum
    if delayInSecond is not negative then use this value as default value otherwise default 180 calls per 15min window limit is ruled

--- src/main/java/com/kadir/twitterbots/ratelimithandler/process/ApiProcessType.java
+++ src/main/java/com/kadir/twitterbots/ratelimithandler/process/ApiProcessType.java
@@ -6,17 +6,27 @@ package com.kadir.twitterbots.ratelimithandler.process;
  * Time: 23:23
  */
 public enum ApiProcessType {
-    SEARCH("search"), SHOW_STATUS("showStatus"), SHOW_USER("showUser"), UPDATE_STATUS("updateStatus"),
-    GET_BLOCKS_IDS("getBlocksIDs"), GET_USER_LISTS("getUserLists"), GET_USER_LIST_MEMBERS("getUserListMembers"),
+    SEARCH("search"), SHOW_STATUS("showStatus"), SHOW_USER("showUser", 1), UPDATE_STATUS("updateStatus"),
+    GET_BLOCKS_IDS("getBlocksIDs"), GET_USER_LISTS("getUserLists"), GET_USER_LIST_MEMBERS("getUserListMembers", 1),
     SHOW_FRIENDSHIP("showFriendship"), CREATE_USER_LIST("createUserList"), CREATE_USER_LIST_MEMBERS("createUserListMembers");
 
     private String name;
+    private int delayInSecond = -1;
 
     private ApiProcessType(String name) {
         this.name = name;
     }
 
+    private ApiProcessType(String name, int delayInSecond) {
+        this.name = name;
+        this.delayInSecond = delayInSecond;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public int getDelayInSecond() {
+        return delayInSecond;
     }
 }
