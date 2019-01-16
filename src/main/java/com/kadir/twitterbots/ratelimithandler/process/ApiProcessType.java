1ffa3b215f2ee747e09e2a6f89f18e625e615620
kadir <akadirkkoc@gmail.com>
2019-01-16 21:58:46 +0300

    Add new ApiProcessTypes

--- src/main/java/com/kadir/twitterbots/ratelimithandler/process/ApiProcessType.java
+++ src/main/java/com/kadir/twitterbots/ratelimithandler/process/ApiProcessType.java
@@ -7,7 +7,8 @@ package com.kadir.twitterbots.ratelimithandler.process;
  */
 public enum ApiProcessType {
     SEARCH("search"), SHOW_STATUS("showStatus"), SHOW_USER("showUser"), UPDATE_STATUS("updateStatus"),
-    GET_BLOCKS_IDS("getBlocksIDs"), GET_USER_LISTS("getUserLists"), GET_USER_LIST_MEMBERS("getUserListMembers");
+    GET_BLOCKS_IDS("getBlocksIDs"), GET_USER_LISTS("getUserLists"), GET_USER_LIST_MEMBERS("getUserListMembers"),
+    SHOW_FRIENDSHIP("showFriendship"), CREATE_USER_LIST("createUserList"), CREATE_USER_LIST_MEMBERS("createUserListMembers");
 
     private String name;
 
