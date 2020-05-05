fe221a402a6740631da527f8d3f657b8ec8288f8
kadir <akadirkkoc@gmail.com>
2020-05-05 22:57:42 +0300

    Add new process type

--- src/main/java/com/kadir/twitterbots/ratelimithandler/process/ApiProcessType.java
+++ src/main/java/com/kadir/twitterbots/ratelimithandler/process/ApiProcessType.java
@@ -9,7 +9,8 @@ public enum ApiProcessType {
     SEARCH("search"), SHOW_STATUS("showStatus", 1), SHOW_USER("showUser", 1), UPDATE_STATUS("updateStatus"),
     GET_BLOCKS_IDS("getBlocksIDs"), GET_USER_LISTS("getUserLists"), GET_USER_LIST_MEMBERS("getUserListMembers", 1),
     SHOW_FRIENDSHIP("showFriendship"), CREATE_USER_LIST("createUserList"), CREATE_USER_LIST_MEMBERS("createUserListMembers"),
-    DESTROY_STATUS("destroyStatus", 1), DESTROY_FAVORITE("destroyFavorite"), GET_USER_TIMELINE("getUserTimeline");
+    DESTROY_STATUS("destroyStatus", 1), DESTROY_FAVORITE("destroyFavorite"), GET_USER_TIMELINE("getUserTimeline"),
+    GET_FRIENDS_IDS("getFriendsIDs");
 
     private String name;
     private int delayInSecond = -1;
